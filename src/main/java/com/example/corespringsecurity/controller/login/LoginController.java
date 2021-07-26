df03ef826660ceb7b4a879acbe00c6b1cc33b8a3
wnstlr0615 <76856000+wnstlr0615@users.noreply.github.com>
2021-07-26 20:07:51 +0900

    로그아웃 설정 및 화면 보안 처리

--- src/main/java/com/example/corespringsecurity/controller/login/LoginController.java
+++ src/main/java/com/example/corespringsecurity/controller/login/LoginController.java
@@ -1,10 +1,16 @@
 package com.example.corespringsecurity.controller.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -17,5 +23,13 @@ public class LoginController {
         model.addAttribute("error", error);
         model.addAttribute("exception", exception);
         return "user/login/login";
+    }
+
+    @GetMapping("/logout")// GET 방식을 사용한 로그아웃 처리리
+   public String logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication!=null)
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        return "redirect:/login";
     }
 }
